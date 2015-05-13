ba2433e0fee20fc9019a773f8b5acf4a3a5173aa
Dmitriy_Rudko <rudko.d.v@gmail.com>
2015-05-13 23:58:00 +0300

    basic structure

---
+++ module1/pluggin/src/main/java/by/rudko/classloading/Pluggin.java
@@ -0,0 +1,11 @@
+package by.rudko.classloading;
+
+/**
+ * Created by rudkodm on 5/13/15.
+ */
+public class Pluggin {
+    
+    public Object run(String...args){
+        return "Hello from pluggin";
+    }
+}
