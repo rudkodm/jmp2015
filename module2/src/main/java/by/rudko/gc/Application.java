cb4b2976ddc86ca03804aaa2703623458e7d26bc
Dmitriy_Rudko <rudko.d.v@gmail.com>
2015-05-21 00:30:43 +0300

    Add some infrastructure logic. Add YoungGen test

--- module2/src/main/java/by/rudko/gc/Application.java
+++ module2/src/main/java/by/rudko/gc/Application.java
@@ -1,31 +1,82 @@
 package by.rudko.gc;
 
-public class Application {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) throws Exception{
-		
-		while(true) {
-			System.out.println(new Bean().getId());
-			Thread.sleep(1);
-		}
-	}
+import static java.lang.System.*;
+
+public class Application {
+    private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        final String key = "test";
+        final String value = System.getProperty(key);
+
+        switch (Command.toEnum(value)) {
+            case TEST_YONG_GEN: {
+                testYong();
+                break;
+            }
+            case TEST_OLD_GET: {
+//                testOld();
+                break;
+            }
+            default: {
+                err.printf("There is no such property %s : %s", key, value);
+            }
+        }
+
+
+    }
+
+    private static void testYong() {
+        while (true) {
+            out.println(new Bean().getId());
+        }
+    }
+
+    private static void testOld() {
+        while (true) {
+            out.println(new Bean().getId());
+        }
+    }
 }
 
 
 class Bean {
-	private static int counter = 0;
-	
-	@SuppressWarnings("unused")
-	private int[] size = new int[1000];
-	
-	private int id = 0;
-	
-	public int getId() {
-		return id;
-	}
+    private static int counter = 0;
 
-	public Bean() {
-		this.id = counter++;
-	}
-	
+    @SuppressWarnings("unused")
+    private int[] size = new int[1000];
+
+    private int id = 0;
+
+    public int getId() {
+        return id;
+    }
+
+    public Bean() {
+        this.id = counter++;
+    }
+
+}
+
+enum Command {
+    TEST_YONG_GEN("young"),
+    TEST_OLD_GET("old");
+
+    private final String value;
+
+    Command(String value) {
+        this.value = value;
+    }
+
+    static Command toEnum(String value) {
+        for (Command command : values()) {
+            if (command.value.equals(value)) {
+                return command;
+            }
+        }
+        return null;
+    }
 }
