211b599650423f7d369c71df96bf1dd78d557c94
Dmitriy_Rudko <Dmitriy_Rudko@epam.com>
2015-05-20 18:43:23 +0300

    Some sample program

--- module2/src/main/java/by/rudko/gc/Application.java
+++ module2/src/main/java/by/rudko/gc/Application.java
@@ -3,10 +3,29 @@ package by.rudko.gc;
 public class Application {
 
 	public static void main(String[] args) throws Exception{
-		int i = 0;
+		
 		while(true) {
-			System.out.println(i++);
-			Thread.sleep(1000);
+			System.out.println(new Bean().getId());
+			Thread.sleep(1);
 		}
 	}
+}
+
+
+class Bean {
+	private static int counter = 0;
+	
+	@SuppressWarnings("unused")
+	private int[] size = new int[1000];
+	
+	private int id = 0;
+	
+	public int getId() {
+		return id;
+	}
+
+	public Bean() {
+		this.id = counter++;
+	}
+	
 }
