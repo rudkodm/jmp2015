6d3f2706490a2c263230c96a60b9289a036848c3
Dmitriy_Rudko <rudko.d.v@gmail.com>
2015-05-27 23:53:35 +0300

    Implement stackOverflow error test

---
+++ module3/src/main/java/by/rudko/memory/StackOverflowTest.java
@@ -0,0 +1,50 @@
+package by.rudko.memory;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class StackOverflowTest {
+    static {
+        BasicConfigurator.configure();
+    }
+
+    private static final Logger LOG = Logger.getLogger(StackOverflowTest.class);
+
+    public static void main(String[] args) throws Exception {
+        LOG.debug(">> Testing StackOverflowError");
+
+        ClassPool cp = ClassPool.getDefault();
+        CtClass cc = cp.get("by.rudko.memory.Methods");
+
+        CtMethod m1 = cc.getDeclaredMethod("method1");
+        m1.insertBefore(buildMethodBody());
+        m1.insertAfter("method2();");
+
+        CtMethod m2 = cc.getDeclaredMethod("method2");
+        m2.insertBefore(buildMethodBody());
+        m2.insertAfter("method3();");
+
+        CtMethod m3 = cc.getDeclaredMethod("method3");
+        m3.insertBefore(buildMethodBody());
+
+        Class c = cp.toClass(cc);
+        ((Methods)c.newInstance()).method1();
+    }
+
+    private static String buildMethodBody() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 3500; i++) {
+            builder.append(String.format("long a%s = %s;", i, i)).append("\r\n");
+        }
+        return builder.toString();
+    }
+}
+
+class Methods {
+    public void method1(){};
+    public void method2(){};
+    public void method3(){};
+}
