da5a3cac64af85c567da5ca35e878f500e1439bc
Dmitriy_Rudko <Dmitriy_Rudko@epam.com>
2015-07-29 17:47:36 +0300

    Module 11. Initial commit

---
+++ module11/jms_publisher/src/main/java/by/rudko/jms/JmsPublisherApplication.java
@@ -0,0 +1,12 @@
+package by.rudko.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsPublisherApplication.class, args);
+    }
+}
