4e16a3c266bf6dca288cea4877e647f8617b7579
Dmitriy_Rudko <rudko.d.v@gmail.com>
2015-07-30 01:56:23 +0300

    Some implementation

--- module11/jms_publisher/src/main/java/by/rudko/jms/JmsPublisherApplication.java
+++ module11/jms_publisher/src/main/java/by/rudko/jms/JmsPublisherApplication.java
@@ -2,11 +2,28 @@ package by.rudko.jms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 
 @SpringBootApplication
 public class JmsPublisherApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JmsPublisherApplication.class, args);
+        ApplicationContext context = SpringApplication.run(JmsPublisherApplication.class, args);
+
+        MessageCreator messageCreator = new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                return session.createTextMessage("ping!");
+            }
+        };
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+        System.out.println("Sending a new message.");
+        jmsTemplate.send("mailbox-destination", messageCreator);
     }
 }
